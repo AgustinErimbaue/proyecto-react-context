31e6c91e65b4a1de39ae5b88d8f13aa73958931b
Agustin Bernabe Erimbaue Lamontanaro <agustibernabe@gmail.com>
2024-05-28 12:42:16 +0200

    se agrega formulario para el login con su estilo correspondiente

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { useContext } from "react";
+import { UserContext } from "../../Context/UserState/UserState";
+import { Form, Input, Button } from "antd";
+import "./Login.scss"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const { login } = useContext(UserContext);
+  const onFinish = (values) => {
+    login(values);
+  };
 
-export default Login
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <div className="container">
+      <Form
+        name="basic"
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
